2157d037081e9ed60375f5e359bbf49cb0dfbc5e
gunal <thirumalaigunal7@gmail.com>
2025-02-20 12:44:15 +0530

    "Added delete function"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,53 @@
+import { useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App(){
+  const [users, setUsers] = useState([]);
+  const [filterUser, setFilterUser] = useState([]);
+  
+  const getAllUsers = async () => {
+    await axios.get("http://localhost:2000/users").then
+    ((res) => {
+      setUsers(res.data);
+      setFilterUser(res.data);
+    });
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  //Search Function
+  const handleSearchChange = (e) => {
+    const SearchText = e.target.value.toLowerCase();
+    const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(SearchText) || user.city.toLowerCase().includes(SearchText));
+    setFilterUser(filteredUsers);
+  }
+
+  //Delete Function
+  const handleDelete = async (id) =>{
+    const isConfirm = window.confirm("ARE YOU SURE YOU WANT TO DELETE THIS USER?");
+    if(isConfirm){
+    await axios.delete(`http://localhost:2000/users/${id}`).
+    then((res) => {
+      setUsers(res.data);
+      setFilterUser(res.data);
+    });
+  }
+  }
+
+  
   return(
     <>
-      <div className="flex justify-center mb-5 border-b-2 border-gray-300">
-        <h3 className="text-xl w-800 p-6">CRUD APPLICATION</h3>
+      <div className=" mb-5 border-b-2 border-gray-300 max-w-[800px] m-auto bg-black text-white">
+        <h3 className="flex justify-center text-xl w-800 p-6">CRUD APPLICATION</h3>
       </div>
-      <div className='input-search'>
-        <input className='outline-black border-2' type="search"/>
+      <div className='flex justify-between items-center mb-6  max-w-[800px] m-auto'>
+        <input className='w-[300px] h-10 border-gray-400 outline-none border-2' type="search" placeholder='Search Text Here' onChange={handleSearchChange}/>
         <button className='btn green'>Add Button</button>
       </div>
-      <table>
+      <table className='table w-[100%] cursor-pointer  max-w-[800px] m-auto'>
         <thead>
           <tr>
             <th>Roll No</th>
@@ -22,30 +59,20 @@ function App(){
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Gunal</td>
-            <td>21</td>
-            <td>Thanjavur</td>
-            <td><button className='btn green'>Edit</button></td>
-            <td><button className='btn red'>Delete</button></td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Gunal</td>
-            <td>21</td>
-            <td>Thanjavur</td>
-            <td><button className='btn green'>Edit</button></td>
-            <td><button className='btn red'>Delete</button></td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Gunal</td>
-            <td>21</td>
-            <td>Thanjavur</td>
-            <td><button className='btn green'>Edit</button></td>
-            <td><button className='btn red'>Delete</button></td>
-          </tr>
+          {filterUser &&
+            filterUser.map((user, index) => {
+              return (
+                <tr key={user.id}>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.age}</td>
+                  <td>{user.city}</td>
+                  <td><button className='btn green'>Edit</button></td>
+                  <td><button onClick={() => handleDelete(user.id)} className='btn red'>Delete</button></td>
+                </tr>
+              )
+            })}
+          
         </tbody>
       </table>
     </>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const cors = require("cors");
 const users = require("./sample.json");
+const fs = require("fs");
 
 const server = express();
 const port = 2000;
 
+server.use(cors());
+
 server.get("/users", (req,res) => {
     return res.json(users);
 });
+
+server.delete("/users/:id", (req,res) => {
+    let id = Number(req.params.id);
+    let filteredUsers = users.filter((user) => user.id !== id);
+    fs.writeFile("./sample.json", JSON.stringify
+        (filteredUsers), (err,data) => {
+        return res.json(filteredUsers);
+    });
+})
 
 server.listen(port, (err) => {
     console.log(`App is running in port ${port}`);
